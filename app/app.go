5775caa595047e09268318425f9fcf2c038bcdc3
Alicio <alicio.alvesm@gmail.com>
2022-07-15 22:15:47 -0300

    added app commands and created function to search ips

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // This function will return the terminal application ready to be executed
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "Domain to IP"
 	app.Usage = "Search IPs and Server names online"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IP addresses online",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
